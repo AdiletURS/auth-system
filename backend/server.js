8e89caa869c55497329fa123343adb453bb41756
Adilet Tursunbekov <adileturs@gmai.com>
2025-06-11 11:44:06 +0600

    Исправленна ошибка версии node в backend/Dockerfile, из-за чего не запускался сервер. Добавленная комада запуска сервера в /docker-compose.yml

--- backend/server.js
+++ backend/server.js
@@ -9,21 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Проверка работы сервера
-app.get('/', (req, res) => {
-    res.send('Auth Server is running');
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-
-
 db.authenticate()
     .then(() => console.log('Database connected'))
-    .catch(err => console.error('Database error:', err));
+    .catch(err => console.error('Database error:', err))
+    .finally(() => {
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    });
 
 // Синхронизация моделей
 db.sync({ alter: true });
@@ -32,3 +26,17 @@ db.sync({ alter: true });
 
 
 app.use('/api/auth', authRoutes);
+
+// Проверка работы сервера
+app.get('/', (req, res) => {
+    res.send('Auth Server is running');
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
