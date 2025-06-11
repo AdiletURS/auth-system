c30365a15af469e2635cb9bc45a0240ba7b117a4
Adilet Tursunbekov <adileturs@gmai.com>
2025-06-11 10:40:45 +0600

    Бэкенд, настройка БД и создание модели Пользователя

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('User', {
+        login: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const db = require('./config/db');
+const User = require('./models/User');
+
+
+db.authenticate()
+    .then(() => console.log('Database connected'))
+    .catch(err => console.error('Database error:', err));
+
+// Синхронизация моделей
+db.sync({ alter: true });
 
 const app = express();
 app.use(cors());
@@ -15,3 +25,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
