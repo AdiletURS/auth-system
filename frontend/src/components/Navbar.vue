f79f32c1c7599fb5ace18017b838f2a705de4a5d
eldegende <eliyaz2761@gmail.com>
2025-06-14 22:20:32 +0600

    Фронтэнд, собираюсь полность переделывать сайт.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -3,74 +3,9 @@
 </script>
 
 <template>
-  <nav class="navbar">
-    <RouterLink class="logo_link" to="/">
-      <img style="filter: invert(100%)" src="@/assets/logo.png" alt="SomeOrdinaryCompany logo">
-      <span>SomeOrdinaryCompany</span>
-    </RouterLink>
 
-    <ul class="links">
-      <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/dashboard">Dashboard</RouterLink></li>
-      <li><RouterLink id="link_login" to="/login">Login</RouterLink></li>
-    </ul>
-  </nav>
 </template>
 
 <style scoped>
-.navbar {
-  padding: 0 20%;
 
-  display: flex;
-  justify-content: space-between;
-
-  background-color: var(--color-background-mute);
-  box-shadow: 0 5px 5px rgba(0, 0, 0, .4);
-}
-
-.logo_link {
-  display: flex;
-  align-items: center;
-  gap: 4px;
-
-  color: white;
-  text-decoration: none;
-}
-.logo_link img {
-  width: 60px;
-}
-
-.links {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-
-  list-style: none;
-}
-.links a {
-  display: inline-block;
-  height: 55px;
-
-  color: white;
-  text-decoration: none;
-  line-height: 55px;
-}
-.links a:hover {
-  text-decoration: underline;
-}
-
-#link_login {
-  width: 80px;
-  height: 40px;
-  text-align: center;
-
-  background-color: var(--vt-c-indigo);
-  border-radius: 100px;
-
-  text-decoration: none;
-  line-height: 40px;
-}
-#link_login:hover {
-  font-weight: bold;
-}
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,31 +1,11 @@
 <script setup>
 
-import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <section class="welcome">
-    <div class="text_block">
-      <span>Добро пожаловать в</span>
-      <h1>SomeOrdinaryCompany</h1>
-    </div>
-
-    <RouterLink to="/reg"><button>Зарегистрироваться</button></RouterLink>
-    <p>или <RouterLink to="/login">войти</RouterLink></p>
-  </section>
+  <h1>sup da world</h1>
 </template>
 
 <style scoped>
-.welcome {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 
-  text-align: center;
-}
-
-.text_block {
-  margin-bottom: 12px;
-}
 </style>
